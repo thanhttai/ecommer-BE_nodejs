33628187ad82a4f92effe0a867a3e96acd35c21d
Tai <ngothanhtaiit@gmail.com>
2021-12-18 21:51:30 +0700

    add Access-Control-Allow-Headers and Access-Control-Allow-Method

--- routes/chatbot.api.js
+++ routes/chatbot.api.js
@@ -21,7 +21,9 @@ router.post("/df_text_query",async(req, res) => {
         let responses = await textQuery(req.body.text, req.body.parameters);
         console.log(responses, 'huhuh')
         res.header("Access-Control-Allow-Origin", "*")
-        
+        res.header("Access-Control-Allow-Headers", "access-control-allow-origin,content-type")
+        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE")
+      
         res.send(responses[0].queryResult)
     } catch (error) {
         console.log(error)
@@ -31,6 +33,8 @@ router.post('/api/df_event_query',async(req, res) => {
     try {
         let responses = await eventQuery(req.body.event, req.body.parameters);
         res.header("Access-Control-Allow-Origin", "*")
+        res.header("Access-Control-Allow-Headers", "access-control-allow-origin,content-type")
+        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE")
         res.send(responses[0].queryResult)
     } catch (error) {
         console.log(error)
